09d81b9a6a914badee08b8b61fbd678a04f3c444
docwali777 <drgauvin@mgmcare.org>
2017-07-29 15:30:03 -0400

    prod vs de venv

---
+++ PASSWORDS_PRIVATE_INFO/dev.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  googleClientID: '626116108027-7skbrdh0f251ifalqtbgqbps78k7loo3.apps.googleusercontent.com',
+  googleClientSecret: 'gDlRQuHfY5zlRqcudaSe5NaQ',
+  MONGODB_URI: 'mongodb://person:person@ds127783.mlab.com:27783/udemy-grider',
+  cookieSessionKey: `.skdj0s8s0[98fdgs[0s-s9f0g s]]`
+}
---
+++ PASSWORDS_PRIVATE_INFO/prod.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  MONGODB_URI: process.env.MONGODB_URI,
+  cookieSessionKey: process.env.COOKIE_SESSION_KEY
+}
