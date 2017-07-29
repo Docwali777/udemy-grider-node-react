dd26830c4cee7ca172cf27b6b051ad964deccb7f
docwali777 <drgauvin@mgmcare.org>
2017-07-28 20:52:29 -0400

    first commit

--- AUTH-PASSPORT/userAuth.js
+++ AUTH-PASSPORT/userAuth.js
@@ -1,17 +1,14 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy
-let keys;
 
-if(process.env.NODE_ENV !== 'production'){
-   keys = require('../PASSWORDS_PRIVATE_INFO/keys');
-}
-console.log(process.env.NODE_ENV, keys);
+  const keys = require('../PASSWORDS_PRIVATE_INFO/keys');
+
 
 passport.use(
   new GoogleStrategy(
     {
-  clientID: process.env.googleClientID || keys.googleClientID ,
-  clientSecret: process.env.googleClientSecret || keys.googleClientSecret ,
+  clientID: process.env.googleClientID ,
+  clientSecret: process.env.googleClientSecret  ,
   callbackURL: '/auth/google/callback'
 },(accessToken, refreshToken, profile)=>{
   console.log('accessToken: ',accessToken);
