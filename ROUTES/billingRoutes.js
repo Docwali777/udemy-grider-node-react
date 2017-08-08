61bc58592dc694fce94061e544ab2edd452c7cf6
docwali777 <drgauvin@mgmcare.org>
2017-08-07 23:08:41 -0400

    prod

--- ROUTES/billingRoutes.js
+++ ROUTES/billingRoutes.js
@@ -5,19 +5,21 @@ const requireLogin = require('../Middlewares/requireLogin')
 module.exports = app => {
 
 
-  app.post('/api/stripe', requireLogin,  async (req, res) => {
-    if(!req.user){
-      return res.status(401).send({error: 'You must login'})
-    }
+  app.post('/api/stripe', requireLogin,  (req, res) => {
 
-    const charges = await stripe.charges.create({
+    stripe.charges.create({
         amount: 500,
         currency: 'usd',
         description: '$5 for 5 credits',
         source: req.body.id
+    }, (err, user)=>{
+      if(err){console.log('Charge not sent')}
+      else {
+        req.user.credits += 5
+        const user  = await req.user.save()
+        res.send(user)
+      }
     })
-    req.user.credits += 5
-    const user  = await req.user.save()
-    res.send(user)
+
 })
 }
